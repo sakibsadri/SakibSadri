45b3ae8e6d308dde4a33a64660cdb56943d989b3
sakibsadri <sakibsadri7050908164@gmail.com>
2025-01-04 15:10:29 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+tailwind.config={
+    theme:{
+        extend:{
+            gridTemplateColumns:{
+                'auto' : 'repeat(auto-fit, minmax(200px, 1fr))'
+            },
+            fontFamily:{
+                Outfit :["Outfit","sans-serif"],
+                Ovo: ["Ovo","serif"]
+            },
+            animation:{
+                spin_slow: 'spin 6s linear infinite'
+            },
+            colors:{
+                lightHover: "#fcf4ff",
+                darkHover: "#fcf4ff",
+                darkTheme: '#11001F'
+            }
+        }
+    },
+    darkMode: 'selector'
+}
